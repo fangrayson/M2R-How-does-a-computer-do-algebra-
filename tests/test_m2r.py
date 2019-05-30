82f69e5880c0707790428ea7388b55481993b992
Fan Grayson <fg3817@ic.ac.uk>
2019-05-30 22:11:34 +0100

    second attempt to test UnaryMinus

--- tests/test_m2r.py
+++ tests/test_m2r.py
@@ -1,7 +1,40 @@
 #Example of a test
 #Go onto 
-def actual_unary_minus(x):
-    return  -x
+#import symbolic.py
+#Seems to be an indentation error in test_m2r.py
+#def actual_unary_minus(x):
+#    return  -x
 
-def test_func(x):
-    assert symbolic.UnaryMinus(x) == actual_unary_minus(x)
+#def test_func(x):
+#    assert symbolic.UnaryMinus(x) == actual_unary_minus(x)
+#Keep getting the error "No module named 'symbolic'"
+import pytest
+
+class Node: 
+    pass
+     
+class Operator(Node):
+    pass
+    
+class UnitaryOperator(Operator):
+    def __init__(self, a):
+        self.operands = (a)
+        
+    def __str__(self):
+        return str(self.operands[0])
+
+class UnaryMinus(UnitaryOperator):
+    symbol = "-"
+       
+def actual_unary_minus(x):
+    return  str(-x)
+
+def test_func():
+    x =  1
+    assert UnaryMinus(x) == actual_unary_minus(x)
+#Keep getting the error "No module named 'symbolic'"
+    
+#FIRST ERROR: may need to add "pass" or an empty indentation block
+# don't think can have an empty class cus it keeps going weird
+    
+#SECOND ERROR: "fixture x not found"
