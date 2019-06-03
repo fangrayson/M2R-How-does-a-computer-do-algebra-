f50b9f876ffffe0383b172096e9527a7ddc4d3c2
Fan Grayson <fg3817@ic.ac.uk>
2019-06-03 23:41:46 +0100

    Added integer and float binary operations for add, pow, mul, sub, and truediv

--- symbolic/node.py
+++ symbolic/node.py
@@ -15,22 +15,56 @@ class Node:
             return str(other.name)
 
     def __add__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Add(self.name,other)
         return Add(self.name,other.name)
     def __radd__(self,other):
-        return Add(other.name,self.name)
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Add(other, self.name)
+        return Add(self.name,other.name)
     
     def __sub__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Sub(self.name, other)
         return Sub(self.name, other.name)
-    
+    def __rsub__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Sub(other, self.name)
+        return Sub(other, self.name)
     def __mul__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Mul(self.name, other)
         return Mul(self.name, other.name)
-
+    def __rmul__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Mul(other, self.name)
+        return Mul(other, self.name)
     def __truediv__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Div(self.name, other)
         return Div(self.name, other.name) 
-    
+    def __rtruediv__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Div(other, self.name)
+        return Div(other, self.name)
     def __pow__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Pow(self.name, other)
         return Pow(self.name, other.name)
-    
+    def __rpow__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Pow(other, self.name)
+        return Pow(other, self.name)
     def __neg__(self):
         return Neg(self.name)
 
