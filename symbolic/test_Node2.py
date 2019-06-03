2eacfb998548dd9b1f7e4014cc8c5495e5243795
Fan Grayson <fg3817@ic.ac.uk>
2019-06-04 00:17:46 +0100

    Updated test_Node2.py still need to find way of importing symbolic because am just copying and pasting the code from node.py at the minute

--- symbolic/test_Node2.py
+++ symbolic/test_Node2.py
@@ -1,47 +1,68 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 31 12:42:28 2019
-
-@author: aishy
-"""
 import pytest
-class Node:
-    #def __init__(self):
-    #    pass
-    def __add__(self, other):
-        return print(Add(self, other))
-    
-#def __add__(self, other):
-        #return print(Add(self,a , b))
-#    return print(Add(self, other))
-        #return "HELLOOOO" (this was just a test)
-#def __sub__(self, other):
-#    return print(Sub(self, other))
-#def __mul__(self, other):
-#    return print(Mul(self, other))
-#def __div__(self, other):
-#    return print(Div(self, other))
-#def __pow__(self, other):
-#        return print(Pow(self, other))
-""" def parenthesize(self,other):
-        if self.priority>other.priority:
-            return '('+str(other)+')'
-        else:
-            return str(other)"""
-    
-    
-#class Operator(Node):
-    
-'''class UnitaryOperator(Node):
-    def __init__(self, a):
-        self.operands = (a)
-        
-    def __str__(self):
-        return str(self.operands[0])
+# IMPORTING NODE DOESN'T WORK?
 
-class UnaryMinus(UnitaryOperator):
-    symbol = "-"'''
+class Node: 
+    
+    def parenthesize(self,other):
+        if self.priority > other.priority:
+            return '(' + str(other.name)+ ')'
+        else:
+            return str(other.name)
+
+    def __add__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Add(self.name,other)
+        return Add(self.name,other.name)
+    def __radd__(self,other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Add(other, self.name)
+        return Add(self.name,other.name)
+    
+    def __sub__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Sub(self.name, other)
+        return Sub(self.name, other.name)
+    def __rsub__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Sub(other, self.name)
+        return Sub(other, self.name)
+    def __mul__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Mul(self.name, other)
+        return Mul(self.name, other.name)
+    def __rmul__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Mul(other, self.name)
+        return Mul(other, self.name)
+    def __truediv__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Div(self.name, other)
+        return Div(self.name, other.name) 
+    def __rtruediv__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Div(other, self.name)
+        return Div(other, self.name)
+    def __pow__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Pow(self.name, other)
+        return Pow(self.name, other.name)
+    def __rpow__(self, other):
+        if type(other) == int or type(other) == float:
+            self.priority = 0
+            return Pow(other, self.name)
+        return Pow(other, self.name)
+    def __neg__(self):
+        return Neg(self.name)
+
 
     
 class BinaryOperator(Node):
@@ -49,7 +70,21 @@ class BinaryOperator(Node):
         self.operands = (a,b)
     
     def __str__(self):
-        return str(self.operands[0]) + " " + self.symbol + " " + str(self.operands[1])
+        return str(self.operands[0]) + " " + self.symbol\
+    + " " + str(self.operands[1])
+
+    
+class UnitaryOperator(Node):
+    def __init__(self, a):
+        self.operands = (a)
+        
+    def __str__(self):
+        return self.symbol+str(self.operands[0])
+
+class Neg(UnitaryOperator):
+    symbol = "-"
+    priority = 3
+    
     
 class Symbol(Node):
     def __init__(self, name):
@@ -58,20 +93,44 @@ class Symbol(Node):
         
 class Add(BinaryOperator):
     symbol = '+'
-    
+    priority = 1
+    def __init__(self, a, b):
+        self.operands = (a,b)
+        self.name = str(self.operands[0]) + " " + self.symbol + " " + str(self.operands[1])
+
+                    
 class Sub(BinaryOperator):
     symbol = "-"
+    priority = 2
+    def __init__(self, a, b):
+        self.operands = (a,b)
+        self.name = str(self.operands[0]) + " " + self.symbol + " " + str(self.operands[1])
     
+        
 class Mul(BinaryOperator):
     symbol = "*"
+    priority = 4
+    def __init__(self, a, b):
+        self.operands = (a,b)
+        self.name = str(self.operands[0]) + " " + self.symbol + " " + str(self.operands[1])
     
+        
 class Div(BinaryOperator):
     symbol = "/"
+    priority = 5
+    def __init__(self, a, b):
+        self.operands = (a,b)
+        self.name = str(self.operands[0]) + " " + self.symbol + " " + str(self.operands[1])
+    
     
 class Pow(BinaryOperator):
-    symbol = "**"  
+    symbol = "**" 
+    priority = 6
+    def __init__(self, a, b):
+        self.operands = (a,b)
+        self.name = str(self.operands[0]) + " " + self.symbol + " " + str(self.operands[1])
     
-    
+
 x = Symbol('x').name
 y = Symbol('y').name
 z = Symbol('z').name
