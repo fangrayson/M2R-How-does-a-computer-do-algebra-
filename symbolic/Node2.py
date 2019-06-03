38c42f30c4301a35070385f5b19bf06010b4af1f
henrypalasciano <henry.palasciano17@imperial.ac.uk>
2019-06-03 11:17:00 +0100

    Added unary minus

--- symbolic/Node2.py
+++ symbolic/Node2.py
@@ -1,13 +1,26 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 31 12:42:28 2019
-
-@author: aishy
-"""
 
 class Node: 
-    pass
+    
+    def __add__(self, other):
+        return Add(self,other)
+    def __radd__(self,other):
+        return Add(other,self)
+    
+    def __sub__(self, other):
+        return Sub(self, other)
+    
+    def __mul__(self, other):
+        return Mul(self, other)
+    
+    def __div__(self, other):
+        return Div(self, other) 
+    
+    def __pow__(self, other):
+        return Pow(self, other)
+    
+    def __neg__(self):
+        return Neg(self)
+
 """ def parenthesize(self,other):
         if self.priority>other.priority:
             return '('+str(other)+')'
@@ -15,17 +28,6 @@ class Node:
             return str(other)"""
     
     
-#class Operator(Node):
-    
-'''class UnitaryOperator(Node):
-    def __init__(self, a):
-        self.operands = (a)
-        
-    def __str__(self):
-        return str(self.operands[0])
-
-class UnaryMinus(UnitaryOperator):
-    symbol = "-"'''
 
     
 class BinaryOperator(Node):
@@ -36,6 +38,19 @@ class BinaryOperator(Node):
         return str(self.operands[0]) + " " + self.symbol\
     + " " + str(self.operands[1])
     
+#class Operator(Node):
+    
+class UnitaryOperator(Node):
+    def __init__(self, a):
+        self.operands = (a)
+        
+    def __str__(self):
+        return self.symbol+str(self.operands[0])
+
+class Neg(UnitaryOperator):
+    symbol = "-"
+    
+    
 class Symbol(Node):
     def __init__(self, name):
         self.name = name
@@ -43,45 +58,25 @@ class Symbol(Node):
         
 class Add(BinaryOperator):
     symbol = '+'
-    def __add__(self, other):
-        return Add(self,other)
-                
-   
-    
-    
+
+                    
 class Sub(BinaryOperator):
     symbol = "-"
     
-    
-    def __sub__(self, other):
-        return Sub(self, other) 
-   
-    
+        
 class Mul(BinaryOperator):
     symbol = "*"
     
-    
-    def __mul__(self, other):
-        return Mul(self, other)
-
-    
+        
 class Div(BinaryOperator):
     symbol = "/"
     
-    
-    def __div__(self, other):
-        return Div(self, other) 
-
     
 class Pow(BinaryOperator):
     symbol = "**"  
     
     
-    def __pow__(self, other):
-        return Pow(self, other)
-    
-    
-    
+   
 x = Symbol('x').name
 y = Symbol('y').name
 z = Symbol('z').name    
