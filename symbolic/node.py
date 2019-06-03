e536bc5f51164a8733b0805f96a4ece7a39faa5b
henrypalasciano <henry.palasciano17@imperial.ac.uk>
2019-06-03 15:08:49 +0100

    update

--- symbolic/node.py
+++ symbolic/node.py
@@ -15,13 +15,9 @@ class Node:
             return str(other.name)
 
     def __add__(self, other):
-        if type(other) == int or type(other) == float:
-            return Add(self.name,other)
         return Add(self.name,other.name)
     def __radd__(self,other):
-        if type(other) == int or type(other) == float:
-            return Add(other, self.name)
-        return Add(self.name,other.name)
+        return Add(other.name,self.name)
     
     def __sub__(self, other):
         return Sub(self.name, other.name)
