62e96c1941d9769d109876980b18d3dbfae4a5c9
Fan Grayson <fg3817@ic.ac.uk>
2019-06-04 18:34:24 +0100

    addition of setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#SETUP INFORMATION
+from setuptools import setup
+
+setup(
+      name = 'symbolic',
+      version = '1.0',
+      description = 'A symbolic algebra module',
+      packages=['symbolic']
+      )
