f57534b2acedc7da8d369a6df4d64646a4ca9e35
Fan Grayson <fg3817@ic.ac.uk>
2019-06-06 12:01:09 +0100

    Added if to stop the loop in post_traversal

--- symbolic/differentiate.py
+++ symbolic/differentiate.py
@@ -7,20 +7,16 @@ Created on Wed Jun  5 11:31:10 2019
 """
 
 def post_traversal(expr,fn):
-    return fn(expr,tuple(post_traversal(o,fn) for o in expr.operands))
-
+    if (isinstance(expr, Symbol) or isinstance(expr, Numberclass) or isinstance(expr, Number)):
+        return fn(expr, 0)
+    else:
+        return fn(expr,tuple(post_traversal(o,fn) for o in expr.operands))
 
 from functools import singledispatch
 @singledispatch
 def diff(node,doperands):
-    if (isinstance(node,Symbol)==True and node!=x) or isinstance(node,Numberclass)==True:
-        return 0
-    elif node==x:
-        return 1
-    
-
-
-    
+    pass
+      
 @diff.register(Add)
 def _(node,doperands):
     return doperands[0]+doperands[1]
@@ -31,11 +27,11 @@ def _(node,doperands):
 
 @diff.register(Mul)
 def _(node,doperands):
-    return doperands[0]*Node.operands[1]+Node.operands[0]*doperands[1]
+    return doperands[0]*node.operands[1]+node.operands[0]*doperands[1]
 
 @diff.register(Div)
 def _(node,doperands):
-    return (doperands[0]*Node.operands[1]-Node.operands[0]*doperands[1])/(Node.operands[1]**2)
+    return (doperands[0]*node.operands[1]-node.operands[0]*doperands[1])/(node.operands[1]**2)
 
 @diff.register(Pow)
 def _(node,doperands):
@@ -53,3 +49,4 @@ def _(node, doperands):
 def _(node, doperands):
     if node == x:
         return 1
+    return 0
