2b3273bd1b84557f803106af4c57ef5d52e3c676
aishychid <aishwarya.chidambaram17@ic.ac.uk>
2019-06-08 17:11:34 +0100

    fix terminal name

--- symbolic/tree_visualiser.py
+++ symbolic/tree_visualiser.py
@@ -11,18 +11,17 @@ z = sn.Symbol('z')
 def post_traversal(expr,fn):
     return fn(expr,tuple(post_traversal(o,fn) for o in expr.operands))
 
-
+dot=Digraph(comment='Abstract Syntax Tree')
 
 from functools import singledispatch
 @singledispatch
 def visitor_function(expr,voperands):
-    dot=Digraph(comment='Abstract Syntax Tree')
     count=0
     
     @visitor_function.register(sn.Operator)
     def _(expr,child_node_names):
         count+=1
-        node_name="N%d"%count
+        node_name="N%d" %count
         dot.node(node_name,expr.symbol)
         for n in child_node_names:
             dot.edge(node_name,n)
@@ -30,8 +29,9 @@ def visitor_function(expr,voperands):
     
     @visitor_function.register(sn.Terminal)
     def _(expr,child_node_names):
-        node_name=str(expr.name)
-        dot.node(node_name,expr.name)
+        count += 1
+        node_name="N%d" %count
+        dot.node(node_name,str(expr.name))
         return node_name
         
     return dot.source
