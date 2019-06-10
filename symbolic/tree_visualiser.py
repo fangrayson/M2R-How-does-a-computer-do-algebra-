9808ac0770472cb64600168cf2c1b1a677a90643
henrypalasciano <henry.palasciano17@imperial.ac.uk>
2019-06-10 11:54:02 +0100

    moved count

--- symbolic/tree_visualiser.py
+++ symbolic/tree_visualiser.py
@@ -13,26 +13,27 @@ def post_traversal(expr,fn):
 
 dot=Digraph(comment='Abstract Syntax Tree')
 
-from functools import singledispatch
-@singledispatch
-def tree_visualiser(expr,child_node_names):
+def tree_visualiser(expr):
     count=0
-    return dot.source
+    from functools import singledispatch
+    @singledispatch
+    def tree_visitor(expr,child_node_names):
+        pass
 
-@tree_visualiser.register(sn.Operator)
-def _(expr,child_node_names):
-    count+=1
-    node_name="N%d" %count
-    dot.node(node_name,expr.symbol)
-    for n in child_node_names:
-        dot.edge(node_name,n)
+    @tree_visitor.register(sn.Operator)
+    def _(expr,child_node_names):
+        count+=1
+        node_name="N%d" %count
+        dot.node(node_name,expr.symbol)
+        for n in child_node_names:
+            dot.edge(node_name,n)
+            return node_name
+
+    @tree_visitor.register(sn.Terminal)
+    def _(expr,child_node_names):
+        count+=1
+        node_name="N%d" %count
+        dot.node(node_name,expr.symbol)
         return node_name
-
-@tree_visualiser.register(sn.Terminal)
-def _(expr,child_node_names):
-    count+=1
-    node_name="N%d" %count
-    dot.node(node_name,expr.symbol)
-    return node_name
-        
+    return post_traversal(expr, tree_visitor)
     
